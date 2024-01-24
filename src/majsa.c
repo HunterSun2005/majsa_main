8f9f50e61fe5acd0675bf0dfd4fa89d5a466a918
Hunter <2396179505@qq.com>
2024-01-24 20:52:53 +0800

    compile

--- src/majsa.c
+++ src/majsa.c
@@ -91,7 +91,6 @@ Result *majsa(Status *status) {
         } else if (result->type == RON) {
             result->point[status->currentPlayer] = Possibles->Situations[max].point + status->honbaCount * 300;
         }
-        result->han--;
         return result;
     }       //可以和牌
     else {
